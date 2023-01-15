ff07ff34bedb10576bfbe41a12ed858514d226dd
macos <chisometi@gmail.com>
2023-01-15 15:35:11 +0100

    first commit

--- lib/features/dashboard/screens/home.dart
+++ lib/features/dashboard/screens/home.dart
@@ -1,4 +1,7 @@
+import 'package:apex_task/features/authentication/view_model/auth_view_model.dart';
+import 'package:apex_task/res/text_stlye.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatelessWidget {
   static const String routeName = "Dashboard";
@@ -6,8 +9,11 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AuthViewModel authViewModel = context.watch<AuthViewModel>();
+    var user = authViewModel.userModel!.data!.user!;
     return Scaffold(
-
+       body: Center(child: Text("Welcome ${user.username!}",
+       style: AppTextTheme.h1,)),
     );
   }
 }
