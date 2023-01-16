a8c650a65583167dc0ed9571c588748e4cf4a3db
macos <chisometi@gmail.com>
2023-01-16 10:07:03 +0100

    first commit

--- lib/common/base_view_model.dart
+++ lib/common/base_view_model.dart
@@ -10,11 +10,15 @@ class BaseViewModel extends ChangeNotifier {
   String _errorMessage = "";
   String get errorMessage => _errorMessage;
 
+
+  /// the function is used to set the error message of the app on a viewModel
   void setErrorMessage(String msg) {
     _errorMessage = msg;
     notifyListeners();
   }
 
+
+/// the function is used to set the the state of the app on a viewModel
   void setState(AppState state) {
     _appState = state;
     notifyListeners();
